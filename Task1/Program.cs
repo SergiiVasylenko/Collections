91d21fd99fe927a568a5bb4095378facbcc89018
sergii <servasylenko@deloitte.com>
2021-05-25 11:09:03 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static readonly string Path1 = @"C:\Users\Sergii\Desktop\Collection\Task1.txt";
+        static void Main(string[] args)
+        {
+            var text = ReadFile(Path1);
+            var listTime = TimeList(text);
+            var linkedListTime = TimeLinkedList(text);
+
+            Console.WriteLine(" List Time -> {0}, LinkedList Time -> {1}.", listTime, linkedListTime);
+
+            Console.ReadKey();
+        }
+
+        static string[] ReadFile(string path)
+        {          
+            string[] text = File.ReadAllLines(path);
+            return text;
+        }
+
+        static long TimeList(string[] text)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var list = new List<string>();
+
+            foreach (var item in text)
+            {
+                list.Add(item);
+            }
+
+            stopWatch.Stop();
+
+            return stopWatch.ElapsedMilliseconds;
+        }
+
+        static long TimeLinkedList(string[] text)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var list = new LinkedList<string>();
+
+            foreach (var item in text)
+            {
+                list.AddFirst(item);
+            }
+
+            stopWatch.Stop();
+
+            return stopWatch.ElapsedMilliseconds;
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static readonly string Path2 = @"C:\Users\Sergii\Desktop\Collection\Task2.txt";
+        static void Main(string[] args)
+        {
+            var text = ReadFile(Path2);
+            var list = GetWords(text);
+
+            var topTenRepeatWords = list.OrderByDescending(x => x.Value).Take(10);
+
+            foreach (var item in topTenRepeatWords)
+            {
+                Console.WriteLine("Word -> {0}, Count -> {1}", item.Key, item.Value);
+            }
+
+            Console.ReadKey();
+        }
+
+        static string[] ReadFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            string[] mystring = noPunctuationText.Split(' ');
+            return mystring;
+        }
+
+        static List<WordObject> GetWords(string[] text)
+        {
+            var list = new List<WordObject>();
+            WordObject item;
+            foreach (var word in text)
+            {
+                item = list.FirstOrDefault(x => x.Key == word);
+                if (item != null)
+                {
+                    item.Value++; 
+                }
+                else
+                {
+                    list.Add(new WordObject { Key = word, Value = 1 });
+                }
+            }
+
+            return list;
+        }
+    }
+
+    class WordObject
+    {
+        public string Key { get; set; }
+        public long Value { get; set; }
+    }
+}
